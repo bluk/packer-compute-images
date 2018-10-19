df8a83760c9c5c7c2b2c5bd81c02fb08b2e9a5c9
Bryant Luk <bryant.luk@bryantluk.com>
2018-10-19 16:26:24 -0500

    Install current security updates during build

--- packer/script/sshd.sh
+++ packer/script/sshd.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -eux
 
-mv /tmp/sshd_config /etc/ssh/sshd_config
-chmod 644 /etc/ssh/sshd_config
-
-awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
-wc -l "${HOME}/moduli" # make sure there is something left
-mv "${HOME}/moduli" /etc/ssh/moduli
-
 mv /tmp/sudoers-ssh-forward-agent /etc/sudoers.d/
 chmod 0440 /etc/sudoers.d/sudoers-ssh-forward-agent
 chown root:root /etc/sudoers.d/sudoers-ssh-forward-agent
--- packer/script/update.sh
+++ packer/script/update.sh
@@ -29,14 +29,16 @@ if [[ $DISTRIB_RELEASE == 18.04 ]]; then
     echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 fi
 
-
 echo "==> Updating list of repositories"
 # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
 apt-get -y update
 
+unattended-upgrade
+
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Performing dist-upgrade (all packages and kernel)"
     apt-get -y dist-upgrade --force-yes
-    reboot
-    sleep 60
 fi
+
+reboot
+sleep 60
