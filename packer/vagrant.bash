64400a4ec44f11971d9ee1bab5b6c6118695364b
Bryant Luk <bryant.luk@bryantluk.com>
2018-11-25 21:01:30 -0600

    Add VMWare Fusion support

---
+++ packer/vagrant.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+vagrant box remove ubuntu1804 --provider virtualbox || true
+vagrant box add ubuntu1804 box/virtualbox/ubuntu1804-0.1.0.box || true
+vagrant box remove ubuntu1804 --provider vmware || true
+vagrant box add ubuntu1804 box/vmware/ubuntu1804-0.1.0.box
