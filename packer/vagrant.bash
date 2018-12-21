37fe4a59b253fa51bacf43f160d64e19b20ca007
Bryant Luk <bryant.luk@bryantluk.com>
2018-12-20 23:06:19 -0600

    Fix vagrant script and ansible config roles path

--- packer/vagrant.bash
+++ packer/vagrant.bash
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 vagrant box remove ubuntu1804 --provider virtualbox || true
 vagrant box add ubuntu1804 box/virtualbox/ubuntu1804-0.1.0.box || true
-vagrant box remove ubuntu1804 --provider vmware || true
+vagrant box remove ubuntu1804 --provider vmware_desktop || true
 vagrant box add ubuntu1804 box/vmware/ubuntu1804-0.1.0.box
